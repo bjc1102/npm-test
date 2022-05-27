961cf7f88965d765e0fcfe11ee6670fd1f24ccd3
byung ju choi <bjc30849@gmail.com>
2022-05-27 14:40:45 +0900

    npm 패키지 테스트

---
+++ src/components/Button.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export interface IPageProps extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+    backgroundColor?: string;
+    color?: string;
+}
+
+export const Button: React.FunctionComponent<IPageProps> = (props) => {
+    const { children, backgroundColor, color, style } = props;
+
+    let _style: React.CSSProperties = style || {};
+    if (backgroundColor) _style.backgroundColor = backgroundColor;
+    if (color) _style.color = color;
+
+    return (
+        <button style={_style} {...props}>
+            {children}
+        </button>
+    );
+};
